ed3b4f98a63ae959d2932ae1785228bf00a89624
sparshshivhare2007-pixel <sparshshivhare2007@gmail.com>
2025-09-27 15:19:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "26407665"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "f10822a551f38b26ee1179e1a6515e01"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
-SUPPORT_GRP = "Ur_support07"
-UPDATE_CHNL = "Ur_rishu_143"
-OWNER_USERNAME = "TheRishuCoder"
+OWNER_ID = int(getenv("OWNER_ID", "8158429107"))
+SUPPORT_GRP = "promXSH"
+UPDATE_CHNL = "promXSH"
+OWNER_USERNAME = "hye_babu"
